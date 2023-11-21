898bf746d07fe1895b35144b2b7426def8860be6
JakubCiolek <kubaciolek383@gmail.com>
2023-11-21 22:51:54 +0100

    Lab3

--- My project/Assets/Scripts/human.cs
+++ My project/Assets/Scripts/human.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,13 +8,14 @@ using UnityEngine.AI;
 public class human : MonoBehaviour
 {
      private float timer = 0f;
-    private float interval = 2f; //Random.Range(1,3);
+    private float interval = 5f; //Random.Range(1,3);
     public NavMeshAgent agent;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        agent.SetDestination(new UnityEngine.Vector3 (Random.Range(-20f,20f),0f,Random.Range(-20f,20f) ));
     }
 
     void Update()
@@ -26,17 +28,18 @@ public class human : MonoBehaviour
         timer += Time.deltaTime;
         if (timer >= interval)
         {
-            agent.SetDestination(RandomNavmeshLocation(1000f));
+            agent.SetDestination(new UnityEngine.Vector3 (Random.Range(0f,10f),0f,Random.Range(0f,10f) ));
+            //agent.SetDestination(RandomNavmeshLocation(10000f));
             timer = 0f;
         }
     }
 
 
-public Vector3 RandomNavmeshLocation(float radius) {
-        Vector3 randomDirection = Random.insideUnitCircle * radius;
+public UnityEngine.Vector3 RandomNavmeshLocation(float radius) {
+        UnityEngine.Vector3 randomDirection = Random.insideUnitCircle * radius;
         randomDirection += transform.position;
         NavMeshHit hit;
-        Vector3 finalPosition = Vector3.zero;
+        UnityEngine.Vector3 finalPosition = UnityEngine.Vector3.zero;
         if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1)) {
             finalPosition = hit.position;
         }
